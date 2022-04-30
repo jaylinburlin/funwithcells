bf9a4da192c48e6163813a7f08187867a061e516
Jaylin Burlin <jaylin.burlin@okbu.edu>
2022-04-30 14:52:36 -0500

    Added questions for each element

---
+++ funwithcells-master/Assets/Buttonscript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Buttonscript : MonoBehaviour
+{
+
+    public GameObject[] Choices;
+
+    public int choiceMade;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ funwithcells-master/README.md
@@ -0,0 +1,4 @@
+# funwithcells
+Laramie, Zach, Jaylin, and Emma Computing 2 Project
+
+This is where all program files for Unity will go as we make changes.
